a99c53315f4b7fc8c1575bf4a5ea697dac4a785a
Dan Romans <danieljromans@gmail.com>
2019-07-24 00:23:56 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,8 +7,8 @@ def introduction_with_language(name, language)
 end
 
 def introduction_with_language_optional(name, language)
-  unless language == NIL
+  if language == NIL
     puts "Hi, my name is #{name} and I am learning to program in Ruby."
-  end
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  else
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
